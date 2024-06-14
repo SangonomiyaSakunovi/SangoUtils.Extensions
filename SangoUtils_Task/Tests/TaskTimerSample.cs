5b8b29f7d82be2493c12f548e740d747c4fdfc9d
Satiya Infomi Omoith <1602274566@qq.com>
2024-06-14 19:06:02 +0800

    update

---
+++ SangoUtils_Task/Tests/TaskTimerSample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SangoUtils.Tasks.Tests
+{
+    internal class TaskTimerSample
+    {
+        private TaskAsyncTimer? _taskAsyncTimer;
+        private TaskTickTimer? _taskTickTimer;
+        private TaskFrameTimer? _taskFrameTimer;
+
+        public void Initialize()
+        {
+            _taskAsyncTimer = new TaskAsyncTimer(true, 16);
+            _taskAsyncTimer.LogInfoFunc = null;
+            _taskAsyncTimer.LogErrorFunc = null;
+            _taskAsyncTimer.LogWarningFunc = null;
+
+            //_taskTickTimer = new TaskTickTimer(true, 10);
+            //_taskTickTimer.LogInfoFunc = null;
+            //_taskTickTimer.LogErrorFunc = null;
+            //_taskTickTimer.LogWarningFunc = null;
+
+            //_taskFrameTimer = new TaskFrameTimer();
+            //_taskFrameTimer.LogInfoFunc = null;
+            //_taskFrameTimer.LogErrorFunc = null;
+            //_taskFrameTimer.LogWarningFunc = null;
+        }
+
+        public uint AddTaskTimer<T>(uint delayedInvokeTaskTime, Action<uint> onTaskUpdated, Action<uint> onTaskCompleted, Action<uint> onTaskCanceled, int repeatTaskCount = 1) where T : TaskBaseTimer => typeof(T).Name switch
+        {
+            nameof(TaskAsyncTimer) => _taskAsyncTimer!.AddTask(delayedInvokeTaskTime, onTaskUpdated, onTaskCompleted, onTaskCanceled, repeatTaskCount),
+            nameof(TaskTickTimer) => _taskTickTimer!.AddTask(delayedInvokeTaskTime, onTaskUpdated, onTaskCompleted, onTaskCanceled, repeatTaskCount),
+            nameof(TaskFrameTimer) => _taskFrameTimer!.AddTask(delayedInvokeTaskTime, onTaskUpdated, onTaskCompleted, onTaskCanceled, repeatTaskCount),
+            _ => 0
+        };
+
+        public bool RemoveTaskTimer<T>(uint taskID) where T : TaskBaseTimer => typeof(T).Name switch
+        {
+            nameof(TaskAsyncTimer) => _taskAsyncTimer!.RemoveTask(taskID),
+            nameof(TaskTickTimer) => _taskTickTimer!.RemoveTask(taskID),
+            nameof(TaskFrameTimer) => _taskFrameTimer!.RemoveTask(taskID),
+            _ => false
+        };
+
+        public bool ResetTaskTimer<T>() where T : TaskBaseTimer => typeof(T).Name switch
+        {
+            nameof(TaskAsyncTimer) => _taskAsyncTimer!.ResetTaskTimer(),
+            nameof(TaskTickTimer) => _taskTickTimer!.ResetTaskTimer(),
+            nameof(TaskFrameTimer) => _taskFrameTimer!.ResetTaskTimer(),
+            _ => false
+        };
+
+        private void Update()
+        {
+            _taskAsyncTimer?.HandleTask();
+            //_taskTickTimer?.HandleTask();
+        }
+
+        public void UpdateByFrame()
+        {
+            //_taskFrameTimer?.HandleTask();
+        }
+    }
+}
--- SangoUtils_Task/obj/Debug/netstandard2.1/SangoUtils.Tasks.AssemblyInfo.cs
+++ SangoUtils_Task/obj/Debug/netstandard2.1/SangoUtils.Tasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SangonomiyaSakunovi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221d73c7ee0ea3cd6748a08949026f1057e93057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3097e1d23ec4472ab773c48ef7a886cf61f735a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SangoUtils.Extensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SangoUtils.Tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
